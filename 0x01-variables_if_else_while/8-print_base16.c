f51501b753b9ed2fbbc4cadda584a0ea51aa9351
Mercyhanu <mercyhanu2016@gmail.com>
2022-10-14 16:53:06 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,38 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Muhammed Abdulrahaman Adinoyi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+alx
