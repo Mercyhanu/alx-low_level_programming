71e56c7cacf91e072b180ed75521ed5f892bca15
Mercyhanu <mercyhanu2016@gmail.com>
2022-10-18 07:54:33 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,23 @@ int main(void)
 {
 
 		-putchar("-");
+
 		-putchar("p");
+
 		-putchar("u");
+
 		-putchar("t");
+
 		-putchar("c");
+
 		-putchar("h");
+
 		-putchar("a");
+
 		-putchar("r");
+
 		-putchar("/n")
+
 			return (0);
 
 }
