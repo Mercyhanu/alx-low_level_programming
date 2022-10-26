b6403c714254c33db806e609436135b93d3638c7
Mercyhanu <mercyhanu2016@gmail.com>
2022-10-26 06:40:34 -0700

    task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,6 +10,8 @@
   *
   * @n: The maximum number of bytes to copied from src.
   *
+  * Desciption: Function that copies a string
+  *
   * Return: A pointer to the resulting string dest.
   */
 char *_strncpy(char *dest, char *src, int n)
