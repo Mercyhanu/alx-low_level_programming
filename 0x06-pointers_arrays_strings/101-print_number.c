730014fb062be03b36cf62c116d4dcf2169c3b98
Mercyhanu <mercyhanu2016@gmail.com>
2022-10-27 07:52:18 -0700

    task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Main - print_numbers - function that print integer numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	print_numbers('98');
+	_putchar('/n');
+	print_numbers('42');
+	_putchar('/n');
+	print_numbers('1040');
+	_putchar('/n');
+	print_numbers('0');
+	_putchar('/n');
+
+	return (0);
+}
+
+
